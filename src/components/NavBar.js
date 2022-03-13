0a4ba4438523f659dda9bc8169f1835342583d52
marisasaunders <marisaksaunders@gmail.com>
2022-03-13 00:22:48 -0500

    made nav bar code dynamic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkList = links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
 
   return <nav>
-    <a href="#home" key="home">home</a>
-    <a href="#about" key="about">about</a>
-    <a href="#projects" key="projects">projects</a>
+    {linkList}
   </nav>;
 }
+
 
 export default NavBar;
